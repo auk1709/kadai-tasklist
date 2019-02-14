185f4201176899a3cec87b708816246a9316c67f
malvaceae <ankomoja927@gmail.com>
2019-02-14 13:46:06 +0000

    user login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks.all
   end 
   
   def show
@@ -12,7 +14,7 @@ class TasksController < ApplicationController
   end 
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
     
     if @task.save
       flash[:success] = 'Task が正常に投稿されました'
